80484fd8a376f6c1a4be641908ec43b7f87624ad
fannarl <fannarleo95@hotmail.com>
2018-09-04 14:18:51 +0000

    while statement checks to see if new int is larger than previous int

--- verk/dæmatímaverk/dæmatími_git_algrím/max_int.py
+++ verk/dæmatímaverk/dæmatími_git_algrím/max_int.py
@@ -1,5 +1,11 @@
-num_int = int(input("Input a number: "))    # Do not change this line
-# Fill in the missing code
+max_int = 0
+num_int = 0
+while num_int >= 0:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if num_int < 0:
+        break
+    elif num_int > max_int:
+        num_int = max_int
 
 
 print("The maximum is", max_int)    # Do not change this line
