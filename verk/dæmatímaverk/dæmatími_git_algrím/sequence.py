02ccd990675f0b54fcdd1492da69107f06e2e00b
fannarl <fannarleo95@hotmail.com>
2018-09-04 14:39:11 +0000

    First add. Figured out the rules for the sequence

---
+++ verk/dæmatímaverk/dæmatími_git_algrím/sequence.py
@@ -0,0 +1,4 @@
+#reglan bakvið þetta sequence 1, 2, 3, 6, 11, 20, 37 er 1+2 =3, 1+2+3=6,2+3+6=11,3+6+11=20,6+11+20=37,11+20+37=58.
+#Búa til for loopu til að finna þetta út
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
