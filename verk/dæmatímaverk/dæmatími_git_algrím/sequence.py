436d5f764cf7568ce44cba0a93d0800c063ee122
fannarl <fannarleo95@hotmail.com>
2018-09-04 14:56:04 +0000

    sequence.py is done.

--- verk/dæmatímaverk/dæmatími_git_algrím/sequence.py
+++ verk/dæmatímaverk/dæmatími_git_algrím/sequence.py
@@ -8,15 +8,23 @@ newest_int = 0
 #length of sequence
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
-for x in range (0, n):
-    if n = 1:
+for n in range (0, n):
+#harðkóða upphafspunktana   
+    if n == 0:
         int_1 = 1
         print(int_1)
-    elif n = 2:
-        int_1 = 1
+    elif n == 1:
         int_2 = 2
-        int_1 + int_2 = int_3
+        print(int_2)
+    elif n == 2:
+        int_3 = 3
         print(int_3)
+#eftir að fyrstu 3 af sequencinu eru kominn þá getur algrímið tekið við        
     else:
-        int_1 + int_2 + int_3 = newest_int
-        
+        newest_int = int_1 + int_2 + int_3 
+        print(newest_int)
+
+        int_1 = int_2
+        int_2 = int_3
+        int_3 = newest_int
+
