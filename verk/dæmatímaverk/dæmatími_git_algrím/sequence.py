75931f7712154176c5cedc95f7703cd1c4d1bb03
fannarl <fannarleo95@hotmail.com>
2018-09-04 14:47:32 +0000

    working on implementing the algorithm

--- verk/dæmatímaverk/dæmatími_git_algrím/sequence.py
+++ verk/dæmatímaverk/dæmatími_git_algrím/sequence.py
@@ -1,4 +1,22 @@
 #reglan bakvið þetta sequence 1, 2, 3, 6, 11, 20, 37 er 1+2 =3, 1+2+3=6,2+3+6=11,3+6+11=20,6+11+20=37,11+20+37=58.
 #Búa til for loopu til að finna þetta út
 
+int_1 = 0
+int_2 = 0
+int_3 = 0
+newest_int = 0
+#length of sequence
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+for x in range (0, n):
+    if n = 1:
+        int_1 = 1
+        print(int_1)
+    elif n = 2:
+        int_1 = 1
+        int_2 = 2
+        int_1 + int_2 = int_3
+        print(int_3)
+    else:
+        int_1 + int_2 + int_3 = newest_int
+        
